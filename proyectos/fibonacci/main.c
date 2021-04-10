ecbfe86c13984966246d2e84dae89d7da7a44497
juanma <juanmaracho10@gmail.com>
2021-04-10 15:17:39 -0500

    final 'fibonaci.c'

---
+++ proyectos/fibonacci/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float f1 = 10.0f;
+    printf("\nhola soy las letras: %s\n", "abc");
+
+    printf("\nel cuadrado de -1 es: %F", sqrt(-2));
+    return 0;
+}
+
