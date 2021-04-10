642a9caa655049625493a02e842319f340a39dc0
juanma <juanmaracho10@gmail.com>
2021-04-10 15:15:33 -0500

    cometario de prueba

---
+++ proyectos/fibonacci/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float f1 = 10.0f;
+    printf("\nhola soy las letras: %s\n", "abc");
+
+    printf("\nel cuadrado de -1 es: %F", sqrt(-2));
+    return 0;
+}
+
+// comentario de prueba
